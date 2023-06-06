165ed5b59b7ca17cce31de381172f5398d750ce0
blognn_287 <83860336+pikadragonelse@users.noreply.github.com>
2023-06-06 22:22:19 +0700

    add active nav

--- FE/fe-pbl/src/component/header/index.tsx
+++ FE/fe-pbl/src/component/header/index.tsx
@@ -4,9 +4,17 @@ import './index.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
 
+const navMap = {
+    1: 'active-cam',
+    2: 'active-graph',
+    3: 'active-mix',
+    4: 'active-about',
+};
+
 export type Header = { scrollTop?: number };
 export const Header = ({ scrollTop }: Header) => {
     const [isBackground, setIsBackground] = useState<boolean>(false);
+    const [location, setLocation] = useState<string>('');
 
     const handleScrollPage = (scrollTop: number) => {
         if (scrollTop > 50) {
@@ -14,7 +22,6 @@ export const Header = ({ scrollTop }: Header) => {
         } else {
             setIsBackground(false);
         }
-        console.log(123);
     };
 
     useEffect(() => {
@@ -32,23 +39,51 @@ export const Header = ({ scrollTop }: Header) => {
             </div>
             <nav className="nav-container">
                 <ul className="nav-list">
-                    <li className="nav-item">
-                        <a href="#cam-view" className="link-section">
+                    <li className={`nav-item ${location === '1' ? 'active' : ''}`} id="1">
+                        <a
+                            id="1"
+                            href="#cam-view"
+                            className="link-section"
+                            onClick={(event) => {
+                                setLocation(event.currentTarget.id);
+                            }}
+                        >
                             Cam View
                         </a>
                     </li>
-                    <li className="nav-item">
-                        <a href="#graph" className="link-section">
+                    <li className={`nav-item ${location === '2' ? 'active' : ''}`} id="2">
+                        <a
+                            id="2"
+                            href="#graph"
+                            className="link-section"
+                            onClick={(event) => {
+                                setLocation(event.currentTarget.id);
+                            }}
+                        >
                             Graph
                         </a>
                     </li>
-                    <li className="nav-item">
-                        <a href="#synthetic" className="link-section">
+                    <li className={`nav-item ${location === '3' ? 'active' : ''}`} id="3">
+                        <a
+                            id="3"
+                            href="#synthetic"
+                            className="link-section"
+                            onClick={(event) => {
+                                setLocation(event.currentTarget.id);
+                            }}
+                        >
                             Synthetic
                         </a>
                     </li>
-                    <li className="nav-item">
-                        <a href="#about-us" className="link-section">
+                    <li className={`nav-item ${location === '4' ? 'active' : ''}`} id="4">
+                        <a
+                            href="#about-us"
+                            className="link-section"
+                            id="4"
+                            onClick={(event) => {
+                                setLocation(event.currentTarget.id);
+                            }}
+                        >
                             About Us
                         </a>
                     </li>
